c23006f052b4437a348bfee400b49e32125b2424
JeanVecko <JeanVecko@users.noreply.github.com>
2026-09-06 11:00:18 +0100

    Mise en jour SaaS

---
+++ site/app/dashboard/recruteur/page.tsx
@@ -0,0 +1,11 @@
+import SiteHeader from "../../components/SiteHeader";
+import RecruiterDashboard from "../../components/RecruiterDashboard";
+
+export default function DashboardRecruteurPage() {
+  return (
+    <>
+      <SiteHeader active="connexion" />
+      <RecruiterDashboard />
+    </>
+  );
+}
